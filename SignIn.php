8d9dec7ebe2334ce66024295d6c8304b2e1a9e0c
David Adamson <psb12191@uni.strath.ac.uk>
2015-04-28 17:15:39 +0100

    Uploading merged files

--- SignIn.php
+++ SignIn.php
@@ -2,7 +2,6 @@
 $servername = "devweb2014.cis.strath.ac.uk";
 $username = "psb12191";
 $password = "uanoneph";
-
 // Create connection
 $conn = mysql_connect($servername, $username, $password);
 $db = mysql_select_db($username);
@@ -10,7 +9,6 @@ if(!$conn or !$db){
 	echo(mysql_error());
 	die(mysql_error());
 }
-
 //Check if account with given username and password exists
 $query = mysql_query('SELECT * FROM app_accounts WHERE username="'.$_POST['username'].'";');
 $row = mysql_fetch_array($query);
@@ -25,5 +23,4 @@ if($row){
 }else{
 	echo("-1"); // Account could not be found
 }
-
 ?> 
