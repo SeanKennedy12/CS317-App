f752ed61a02348b8fb440fbdc94512de115059b1
ToggleWhenReady <togglewhenready@gmail.com>
2015-04-28 13:26:48 +0100

    Number of small graphical changes

--- setBGC.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-	var new_bgc = document.body.style.backgroundColor;
-	if(docCookies.hasItem('bgc')) {
-		new_bgc = docCookies.getItem('bgc');
-		console.log("Getting saved background color ("+new_bgc+")");
-	}
-	document.body.style.backgroundColor = new_bgc;
-});
